09cd20405f1ff6f13eea49ddc77e74e6b64b106d
Alex Lewin <alexlwn123@gmail.com>
2023-11-14 10:12:53 -0500

    updated timeline

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import 'react-vertical-timeline-component/style.min.css';
-import { School } from '@mui/icons-material'
+import { PermIdentity, School, Bolt, AttachMoney } from '@mui/icons-material'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 
 
 
 export default function Timeline() {
+  // Hacky stuff to center the date
+  const elem = document.getElementsByClassName('vertical-timeline-element-date');
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].style.textAlign = 'center'
+  }
   return (
-    <div id="timeline" className="min-h-screen flex-col items-center text-center m-auto py-20 mt-20">
+    <div id="timeline" className=" text-center m-auto py-20 mt-20">
       <div className="text-5xl font-bold py-8">
         Timeline
       </div>
@@ -19,20 +24,32 @@ export default function Timeline() {
         <VerticalTimelineElement
           contentStyle={{ background: '#0b212d', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid green' }}
-          date="May 2022"
+          date="November 2023"
           iconStyle={{ background: '#0b212d', color: '#fff' }}
-          icon={<School />}
+          icon={<PermIdentity />}
+        >
+          <h3 className="font-large">Zion</h3>
+          <h4 className="vertical-timeline-element-subtitle text-yellow-500">Full Stack Engineer</h4>
+          <p>Building applications involving Decentralized Identity</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          contentStyle={{ background: '#0b212d', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid green' }}
+          date="May 2022"
+          style={{textAlign: 'center'}}
+          iconStyle={{ background: '#0b212d', color: '#fff' }}
+          icon={<Bolt />}
         >
           <h3 className="font-large">BlockSpaces</h3>
           <h4 className="vertical-timeline-element-subtitle text-yellow-500">Full Stack Engineer</h4>
-          <p>My first expxosure to the start-up world. </p>
+          <p>My first expxosure to the start-up world</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           contentStyle={{ background: '#0b212d', color: '#fff' }}
           contentArrowStyle={{ borderRight: '7px solid green' }}
           date="May 2020"
           iconStyle={{ background: '#0b212d', color: '#fff' }}
-          icon={<School />}
+          icon={<AttachMoney />}
         >
           <h3 className="vertical-timeline-element-title">NCR Corporation</h3>
           <h4 className="vertical-timeline-element-subtitle text-yellow-500">Software Engineer</h4>
